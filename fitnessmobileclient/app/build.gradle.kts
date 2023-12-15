b4b0c5b8026ba7d83d7305b0fc51aecce61b6ffd
mwdziak <mwdziak@gmail.com>
2023-12-15 14:02:39 +0100

    Ktor http client dependency added to gradle

--- fitnessmobileclient/app/build.gradle.kts
+++ fitnessmobileclient/app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("io.ktor:ktor-client-core:1.6.4")
+    implementation("io.ktor:ktor-client-android:1.6.4")
+    implementation("io.ktor:ktor-client-json:1.6.4")
+    implementation("io.ktor:ktor-client-serialization:1.6.4")
 }
